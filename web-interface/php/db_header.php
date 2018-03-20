2aed35d3c1cf3dec7a285a0ccd9525c4feaede24
Michael Bowe <michael_bowe@student.uml.edu>
2018-03-20 09:54:31 -0400

    moved db settings to db_header.php so we don't need to pass it around everywhere

---
+++ web-interface/php/db_header.php
@@ -0,0 +1,7 @@
+<?php
+// These are the settings for github. Adjust as needed locally
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "comp3100"; //Change this to the name of your local database
+ ?>
