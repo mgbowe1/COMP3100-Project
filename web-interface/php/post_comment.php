87510d2014b43c9761b0bfc658009cb3d907b080
Michael Bowe <michael_bowe@student.uml.edu>
2018-03-27 10:28:17 -0400

    forgot a .

--- web-interface/php/post_comment.php
+++ web-interface/php/post_comment.php
@@ -7,7 +7,7 @@ if($conn->connect_error) {
   die("Connection to database failed. Failed to post twit");
 }
 $content = $_POST["content"];
-$sql = "INSERT INTO comment (cid, tid, uid, body, comment_time) VALUES (null, " . $_POST["tid"] ", " . $_SESSION["uid"] . ", '" . $content . "', NOW())";
+$sql = "INSERT INTO comment (cid, tid, uid, body, comment_time) VALUES (null, " . $_POST["tid"] . ", " . $_SESSION["uid"] . ", '" . $content . "', NOW())";
 $result = $conn->query($sql);
 header("Location: http://" . $servername . $serverroot . "get_user_feed.php?search=" . $_SESSION["username"]);
 ?>
