53396b618c1cdd9fcc80ff79a574368b91c78ebf
John Perkins <john_perkins@student.uml.edu>
2018-04-04 13:25:06 -0400

    mysqli to $result changes
    changes lines where I used mysqli and changed them to use $result->

--- web-interface/php/index.php
+++ web-interface/php/index.php
@@ -26,10 +26,9 @@ if(!isset($_SESSION["logged_in"])) {
    <?php 
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql = "SELECT body, t1.uid as uid, u1.username as username, t1.post_time as post_time, COUNT(t1.uid) as likes FROM TWITTS t1, THUMB l1, USER u1 WHERE t1.uid = u1.uid AND t1.tid = l1.tid GROUP BY t1.tid ORDER BY COUNT(t1.tid) DESC, t1.post_time ASC LIMIT 1";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
         echo "<div class=\"post row\"><div class=\"col-12 post-inside\"><h2> Top Post - (" . $row["likes"] . ") like(s)</h2></div>";
         echo "<div class=\"col-10 post-inside\"><h3>" . $row["username"] . "</h3></div><div class=\"col post-inside\"></div>";
         echo "<div class=\"col post-inside\"></div><div class=\"col-10 offest post-inside\"><p class=\"tline\">posted at<span class=\"time\" data-time=\"" . $row["post_time"] . "\"> " . $row["post_time"] . "</span></p></div><div class=\"col post-inside\"></div>";
@@ -44,10 +43,9 @@ if(!isset($_SESSION["logged_in"])) {
     <?php 
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql = "SELECT u1.username, COUNT(u1.uid) as followers FROM USER u1, FOLLOW f1 WHERE u1.uid = f1.following_id GROUP BY u1.uid ORDER BY COUNT(u1.uid) DESC, follow_time ASC LIMIT 1";
-    $result = mysqli_query($conn, $sql);
-    
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
         echo "<div class=\"post row\"><div class=\"col-12 post-inside\"><h2> Most Popular User - (" . $row["followers"] . ") follower(s)</h2></div>";
         echo "<div class=\"col-12 post-inside\"><h3>" . $row["username"] . "</h3></div><div class=\"col post-inside\"></div>";
         echo "</div>";
@@ -107,35 +105,15 @@ if(!isset($_SESSION["logged_in"])) {
     </select>
     <input type="submit" value="Unfollow">
    </form>
-   
-<form method="post" action="top_user_year.php">
-  <label for="topofyear">Select Year</label>
-  <select name="topofyear">
-   <?php 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $sql = "SELECT year(post_time) as year FROM TWITTS t1 GROUP BY year(t1.post_time)";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        echo "<option value=\"" . $row["year"] . "\">" . $row["year"] . "</option>";
-      }
-    } else {
-      echo "0 results";
-    }
-    ?>
-    </select>
-    <input type="submit" value="Top User">
-  </form>
 
   <?php 
     $conn = new mysqli($servername, $username, $password, $dbname);
     $sql = "SELECT u2.username, COUNT(*) as number_of_messages FROM USER u2, MESSAGE m2 WHERE u2.uid = m2.sender_id and m2.message_id in (SELECT m1.message_id FROM USER u1, MESSAGE m1 WHERE u1.uid = m1.receiver_id AND u1.uid = " . $_SESSION["uid"] . ") ORDER BY 'send_time' DESC";
-    $result = mysqli_query($conn, $sql);
-    
-    if (mysqli_num_rows($result) > 0) {
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
       echo $row["number_of_messages"] . " Message(s) from: <br>";
-      while ($row = mysqli_fetch_assoc($result)) {
+      while ($row = $result->fetch_assoc()) {
         echo $row["username"] . "<br>";
       }
     } else {
